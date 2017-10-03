11c1bf9b8e0c2f9842e6bad12e5c9bed1f15fc01
Gabriel Custodio <gcmartins93@gmail.com>
2017-10-03 14:46:21 -0300

    added coutry existence verification

--- db/default/spree/country.rb
+++ db/default/spree/country.rb
@@ -1,3 +1,7 @@
-country = Spree::Country.create!(name: 'Brasil', iso3: 'BRA', iso: 'BR', iso_name: 'BRAZIL', numcode: '76', states_required: true)
+unless Spree::Country.exists?(iso_name: 'BRAZIL')
+  country = Spree::Country.create!(name: 'Brasil', iso3: 'BRA', iso: 'BR', iso_name: 'BRAZIL', numcode: '76', states_required: true)
+else
+  country = Spree::Country.where(iso_name: 'BRAZIL').first
+end
 
 Spree::Config[:default_country_id] = country.id
