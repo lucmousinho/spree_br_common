d0d9efce9cdd85670e42c71e6b3c604347cb2447
Gabriel Custodio <gcmartins93@gmail.com>
2017-11-16 16:24:05 -0200

    Added permitted attributes to api

--- app/helpers/spree/api/api_helpers_decorator.rb
+++ app/helpers/spree/api/api_helpers_decorator.rb
@@ -4,6 +4,8 @@ module Spree
       mattr_reader :city_attributes
 
       @@city_attributes = [:id, :name, :ibge_code, :state_id]
+      @@address_attributes.push :district, :number
+      @@user_attributes.push :first_name, :last_name, :cpf, :phone, :alternative_phone, :date_of_birth
 
       class << self
         ATTRIBUTES << :city_attributes
--- app/models/spree/permitted_attributes_decorator.rb
+++ app/models/spree/permitted_attributes_decorator.rb
@@ -1,7 +1,7 @@
 module Spree
   module PermittedAttributes
     class << self
-      @@address_attributes.push :city_id, :district, :number
+      @@address_attributes.push :district, :number
       @@user_attributes.push :first_name, :last_name, :cpf, :phone, :alternative_phone, :date_of_birth
     end
   end
