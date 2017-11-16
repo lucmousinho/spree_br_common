4c0db15c3f76640ea50e0bb615711ce3d6172a9f
Gabriel Custodio <gcmartins93@gmail.com>
2017-11-16 17:29:42 -0200

    added city_id to address

--- app/helpers/spree/api/api_helpers_decorator.rb
+++ app/helpers/spree/api/api_helpers_decorator.rb
@@ -4,7 +4,7 @@ module Spree
       mattr_reader :city_attributes
 
       @@city_attributes = [:id, :name, :ibge_code, :state_id]
-      @@address_attributes.push :district, :number
+      @@address_attributes.push :city_id, :district, :number
       @@user_attributes.push :first_name, :last_name, :cpf, :phone, :alternative_phone, :date_of_birth
 
       class << self
