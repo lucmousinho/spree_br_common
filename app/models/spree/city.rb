8746cbdcff4726825bff75b9387972794ba97134
Gabriel Custódio <gcmartins93@gmail.com>
2018-01-10 16:19:16 -0200

    Update city.rb

--- app/models/spree/city.rb
+++ app/models/spree/city.rb
@@ -1,7 +1,19 @@
-module Spree
-  class City < ActiveRecord::Base
-    belongs_to :state, :class_name => 'Spree::State'
+class Spree::City < Spree::Base
+  has_many :zone_members,
+             -> { where(zoneable_type: 'Spree::City') },
+             class_name: 'Spree::ZoneMember',
+             dependent: :destroy,
+             foreign_key: :zoneable_id
 
-    validates_presence_of :state, :ibge_code, :name
+  has_many :zones, through: :zone_members, class_name: 'Spree::Zone'
+
+  belongs_to :state
+  has_one :country, through: :state
+
+  validates_presence_of :name
+  validates_uniqueness_of :name, scope: [:state_id]
+
+  def state_and_city
+    state ? "#{state.name}- #{name}" : name
   end
 end
